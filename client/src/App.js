6d57c8a6a1f08fa8cabcf5b3fda44d4cc87295b7
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-05-12 14:15:54 +0530

    hii

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <h1>Hello Welcome To MernStack</h1>
+          <h1>Hello Welcome To MernStack by sathys</h1>
     </div>
   );
 }
