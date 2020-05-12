a4e83aebeaab8f19feb5c5eb0b81b3f7072f79a0
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-05-12 13:42:54 +0530

    hii

---
+++ client/build/precache-manifest.f3deb7bf2bb65edd62fe0df752ffde7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182de769bd93df13b4c68466623e379d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b44a5e2e7d90f5a621f6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "500305843aae3fbeed0e",
+    "url": "/static/js/2.207ba51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.207ba51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44a5e2e7d90f5a621f6",
+    "url": "/static/js/main.80a6183a.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app=express();
 const PORT=process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+
+
+
+
 app.get('/', (req, res) => {
     res.send('hiiii');
 });
