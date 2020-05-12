1426d81156966cbf4297bfcaee588489cfc4e1cb
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-05-12 14:39:27 +0530

    hii

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function getresponse () {
+     axios.get('api/sendname').then(res=>{
+       console.log(res.data);
+     })
+     
+  }
+
   return (
     <div className="App">
           <h1>Hello Welcome To MernStack by sathys</h1>
+          <button onClick={getresponse}>get response</button>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const PORT=process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   }
-  
+
+
+  app.get('/api/sendname', (req, res) => {
+      res.send({name:'sathya'});
+  });
+
 app.listen(PORT, () => {
     console.log(`Server started on port`);
 });
