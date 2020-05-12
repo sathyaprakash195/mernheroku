801b31435508df035d8ccc28eea920590e19595a
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-05-12 13:55:03 +0530

    hii

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const PORT=process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   }
-
-
-
-
-
-
+  
 app.listen(PORT, () => {
     console.log(`Server started on port`);
 });
