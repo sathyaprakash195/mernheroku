7affdfbd6cbe5d183917bdeeda9825da63582f6e
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-05-12 20:38:26 +0530

    hgn

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const PORT=process.env.PORT || 5000;
 
   if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('/*', function(req, res) {
-      
-    })
+    
   }
 
 
